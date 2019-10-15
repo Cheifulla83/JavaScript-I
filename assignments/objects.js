ff63e86539fa661599173f5b73e9c5997ece0f82
Blaque <rikeiwilliams@gmail.com>
2019-10-14 20:44:10 -0400

    rikei-williams

--- assignments/objects.js
+++ assignments/objects.js
@@ -68,7 +68,7 @@ console.log(antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 kennan.speak = function () {
-  console.log("Hello, my name is Kennan!" )
+  console.log(`Hello, my name is ${this.name}!` )
 }
 console.log(kennan.speak());
 
@@ -86,7 +86,7 @@ console.log(antonietta.multiplyNums(3,4));
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
-
+const name = {}
 const parent = {
   name: "Susan",
   age: 70,
